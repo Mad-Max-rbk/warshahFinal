2c92da4c34681907d1d4f0b70de33fd41be33f30
a-shamasneh <a.ashmasneh@gmail.com>
2017-02-19 19:17:18 +0200

    server and db ready to received request

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express=require("express");
-var bodyParser=require("body-parser");
-var route=require('./control/route.js');
 var app = express();
+var bodyParser=require("body-parser");
+const mongoose = require('mongoose');
+var route=require('./control/route.js');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 route(app);
+
 var port=(process.env.PORT || 8080);
-app.listen(port);
 console.log('Running on port '+port);
+
+var mongoURI = process.env.MONGODB_URI ||'mongodb://localhost/Warshah';
+mongoose.connect(mongoURI);
+db = mongoose.connection;
+db.once('open',function () {
+	console.log('mongoDB is open');
+});
+
+app.listen(port);
+app.use(express.static(__dirname + '/client'));

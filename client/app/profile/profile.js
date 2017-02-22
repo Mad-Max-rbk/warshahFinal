21a3ade23ef524f4ca55c42b155391bdb0b29f25
AhmadAlMaradni <ahmad.maradni@hotmail.com>
2017-02-22 20:13:23 +0200

    profiley

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -2,22 +2,7 @@ angular.module('app.profile', [])
 
 .controller('ProfileController', function ($scope, $location, $window , User , Tradeworker) {
    $scope.data={};
-   var userID = window.localStorage.getItem('userId');
+   $scope.data.userId = window.localStorage.getItem('userId');
   	
-
-  $scope.getProfile = function(){
-    Tradeworker.getAll()
-    .then(function (data) {
-    	for (var i = 0; i < data.length; i++) {
-    		if(data[i]['s_cid'] === userID){
-      	 		$scope.data = [data[i]];
-      	 		console.log($scope.data)
-    		}
-    	}
-    })
-    .catch(function (error) {
-        console.log(error);
-      });
-  }
 
 })
